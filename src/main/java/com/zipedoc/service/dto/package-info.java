08bcbb8579c9d993033856db714c4818d384dfd9
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-08-25 21:16:22 -0400

    Initial version of ZipEDoc generated by generator-jhipster@7.9.2

---
+++ src/main/java/com/zipedoc/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.zipedoc.service.dto;
---
+++ src/main/java/com/zipedoc/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.zipedoc.web.rest.vm;
