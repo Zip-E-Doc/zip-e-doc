e9fbd0527d4d9fdf20db18751afc37e277a582fe
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-08-31 22:04:05 -0400

    users beyond admin can login

--- src/main/webapp/app/entities/documentview/documentview.tsx
+++ src/main/webapp/app/entities/documentview/documentview.tsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import EditorApp from './EditorApp';
 import DocumentList from './DocumentList';
 import axios from './axios.js';
+import { useAppSelector } from 'app/config/store';
 
 function documentview() {
   const [selectedDocument, setSelectedDocument] = useState('');
   const [config, setConfig] = useState(null);
   const [auth, setAuth] = useState(null);
+  const account = useAppSelector(state => state.authentication.account);
 
   const handleDocumentSelection = data => {
     setSelectedDocument(data);
@@ -21,12 +23,13 @@ function documentview() {
       method: 'post',
       url: `http://localhost:9000/api/authenticate`,
       data: {
-        password: 'admin',
-        username: 'admin',
+        password: account.login,
+        username: account.login,
       },
     }).then(response => {
       setConfig({ headers: { accept: '*/*', Authorization: `Bearer ${response.data.id_token}` } });
       setAuth(response.data.id_token);
+      console.log(account);
     });
     return auth;
   }
